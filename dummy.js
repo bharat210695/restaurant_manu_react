013e396ebaa2553a6b80c399c302741c9aae1ca9
https://github.com/bharat210695 <bharat.aggnibachya@gmail.com>
2023-04-07 00:03:13 +0530

    all

---
+++ dummy.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+// const App = () => {
+//   return React.createElement("h1", {}, "React Page"); // react old approach  - use import react
+// };
+
+// react new approach..............................
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>React Page</h1>
+//       <p>
+//         <h2>Bharat</h2>
+//       </p>
+//     </div>
+//   );
+// }; // using <div> tag.........................
+
+// const App = () => {
+//   return (
+//     <section>
+//       <h1>React Page1</h1>
+//     </section>
+//   );
+// }; // using <section> tag...............................
+
+// const App = () => {
+//   return (
+  
+//     <React.Fragment>
+//       <h1>React Page1</h1>
+//     </React.Fragment>
+//   );
+// }; // using <react fragment> tag
+
+// const App = () => {
+//   return (
+//     <>
+//       <MyName />
+//       <h1 className="React">React Page {6 * 6}</h1>{" "}
+//       <p>Welcome To React Developer</p>
+//       <img src="" alt="" />
+//     </>
+//   ); // using react fragment shortcut tag............
+// };
+
+// // here MyName is a functional component / nested component
+// const MyName = () => {
+//   return <h1>Bharat Aggni Bachya</h1>;
+// };
+
+
+export default App;
